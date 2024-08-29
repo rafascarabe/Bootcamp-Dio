5ff0f8e1c03fa0618b020eb9a7f872d03e9087f8
rafascarabe <rafaela.scarabe@sptech.school>
2024-08-29 16:25:15 -0300

    Criando meu primeiro projeto em React

---
+++ primeiro-projeto-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { //App() = componente
+  return (
+    <div className="App"> 
+    {/* Componente sempre criado com letra maiuscula */}
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
